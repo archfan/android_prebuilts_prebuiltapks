e3c7aa28c3f8829ef241dee68ec5fe52e245c6b2
archfan <ad-min@mailbox.org>
2019-09-02 04:18:25 +0200

    Replace AOSP Calendar with Simple Calendar Pro

--- Calendar/Android.mk
+++ Calendar/Android.mk
@@ -4,7 +4,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := Calendar
 LOCAL_CERTIFICATE := platform
 
-LOCAL_SRC_FILES := Calendar-release-unsigned.apk
+LOCAL_SRC_FILES := com.simplemobiletools.calendar.pro_158.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 include $(BUILD_PREBUILT)
