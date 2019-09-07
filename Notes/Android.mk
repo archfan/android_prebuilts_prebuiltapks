5cb032e3c421d9ff5b614a86ce4bd820dc7d96cd
archfan <ad-min@mailbox.org>
2019-09-07 04:08:23 +0200

    Switch to Simple Notes Pro 6.1.7

--- Notes/Android.mk
+++ Notes/Android.mk
@@ -4,7 +4,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := Notes
 LOCAL_CERTIFICATE := platform
 
-LOCAL_SRC_FILES := app-release-unsigned.apk
+LOCAL_SRC_FILES := com.simplemobiletools.notes.pro_64.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 include $(BUILD_PREBUILT)
