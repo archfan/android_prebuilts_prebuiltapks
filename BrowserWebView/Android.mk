4d04502c34557af76c1ecce19103e2b765a71e0a
0x61 <archfan@users.noreply.github.com>
2019-09-07 04:04:50 +0200

    Update Android.mk

--- BrowserWebView/Android.mk
+++ BrowserWebView/Android.mk
@@ -9,7 +9,7 @@ LOCAL_REQUIRED_MODULES := \
         libwebviewchromium_loader \
         libwebviewchromium_plat_support
 
-	LOCAL_SRC_FILES := arm64_SystemWebView.apk
+LOCAL_SRC_FILES := arm64_SystemWebView.apk
 
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
